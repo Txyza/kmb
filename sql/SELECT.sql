062e051a743b1ea14fc6073b8fb28a38493a7874
Ekaterina Enina <ekate.enina@mail.ru>
2019-07-05 23:00:12 +0700

    Переместила файлы в папку

---
+++ sql/SELECT.sql
@@ -0,0 +1,15 @@
+SELECT * FROM "Users";
+
+SELECT * FROM "Roles";
+
+SELECT * FROM "Products";
+
+SELECT * FROM "Sales";
+
+SELECT * FROM "Detailed_product_description";
+
+SELECT * FROM "Point_of_sale";
+
+SELECT * FROM "Products_at_point_of_sale";
+
+SELECT * FROM "Descrip_point_of_sale";
